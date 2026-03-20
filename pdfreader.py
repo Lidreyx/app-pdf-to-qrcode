731effee0a2a8122356dacc2b3e203fb4c3fad67
Lidreyx <127121808+Lidreyx@users.noreply.github.com>
2026-03-20 11:44:54 +0100

    Fix syntax and improve comments in PDFReader class
    Fix syntax error in extraire_infos_pdf method and add comments for clarity.

--- pdfreader.py
+++ pdfreader.py
@@ -3,28 +3,28 @@ class PDFReader :
     
     def __init__(self) :
         pass
-
-    def extraire_infos_pdf(self, fichier_pdf):
+    # fonction pour récuprer le contenu des champs d'entrés
+    def extraire_infos_pdf(self, fichier_pdf)
         reader = PdfReader(fichier_pdf)
         fields = reader.get_fields()
         valeurs = {}
-
+        #si aucun champs trouvé return
         if not fields:
             print("Aucun champ trouvé dans le PDF.")
             return valeurs
-    
+        #nom des champs d'entré utiliser le code case pour trouver le nom des champs de votre pdf et remplacez les 
         champs_texte = [
             "NOM", "PRENOM", "ORGANISME", "GRADE",
             "LIEUNAISS", "DATNAISS", "H1", "MIN1",
             "NON1", "IMMAT", "OUI1", "NON1", "VEHICULE", "GRADE2", "NOM2", "PRENOM2", "SERVICE", "TEL", "PIECE",
         ]
-
+        #attribut chaque nom de champs à un texte
         for cle in champs_texte:
             if cle in fields:
                 valeurs[cle] = fields[cle].get("/V", "").strip()
 
 
-    
+        #prise en charge des cases à cocher oui ou non
         if "OUI1" in fields and fields["OUI1"].get("/V", "") not in (None, "/OFF"):
             valeurs["pieton"] = "oui"
         elif "NON1" in fields and fields["NON1"].get("/V", "") not in (None, "/OFF"):
